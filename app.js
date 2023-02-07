a2e2f22dcb6dad860d3a9f7fa83e057ea36c6466
jonathan <jonathankendrick697@gmail.com>
2023-02-07 08:45:40 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,45 +1,21 @@
-let navbar=document.querySelector('.nav-settings');
-let floatChat=document.querySelector('.fixed-icon')
-let text=floatChat.querySelector('p');
-let floatingIcon=floatChat.querySelector('i')
-function  myScrollFunction(){
-    let y= window.scrollY
-  
-  if(y>590){
-    floatChat.classList.add('floating-chat-visible')
-  }
-  if(y<590){
-    floatChat.classList.remove('floating-chat-visible')
-  }
-};
+const nav=document.querySelector('.nav');
+const menu=document.getElementById('menu');
+const buttonLeft=document.getElementById('left');
+const buttonRight=document.getElementById('right');
+const specialContainer=document.getElementById("special-container")
 
-
-
-window.addEventListener("scroll", myScrollFunction);
-
-floatingIcon.addEventListener('click',()=>{
-    text.textContent='Sorry we cant really help you this is just a simple landing page LOL!!'
-    let orignalText='Hello,how can we help you?!!';
-    let textCont=text.textContent
-    if(orignalText!==textCont){
-      setTimeout(function(){text.textContent=orignalText},4500)
-    }
-
+menu.addEventListener("click",function(){
+     nav.classList.toggle('open-nav');
+     menu.classList.toggle("change-menu")
 });
 
-const hamburgerMenu=document.querySelector('.menu');
-const navLInks=document.querySelector('.nav-links');
-const closeNav=document.querySelector('.fa-xmark');
-const links=document.querySelector('ul')
-hamburgerMenu.addEventListener('click',()=>{
-    navLInks.classList.toggle('open-links');
 
-   if(navLInks.classList.contains('open-links')){
-    hamburgerMenu.classList.add('change');
-   }else{
-    hamburgerMenu.classList.remove('change');
-   }
-   
-   
-
+buttonRight.addEventListener("click",()=>{
+     
+    specialContainer.scrollLeft +=100
+    console.log(specialContainer.scrollLeft)
 });
+buttonLeft.addEventListener("click",()=>{
+    specialContainer.scrollLeft -= 100
+});
+
