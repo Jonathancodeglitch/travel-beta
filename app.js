4145eae56a1ab1f9432ef7b696f7094d51d6765b
jonathan <jonathankendrick697@gmail.com>
2023-02-08 08:12:26 -0800

    newsletter interaction

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const buttonLeft=document.getElementById('left');
 const buttonRight=document.getElementById('right');
 const specialContainer=document.getElementById("special-container")
 
+const input=document.getElementById("input");
+const inputButton=document.getElementById("input-button");
+
 menu.addEventListener("click",function(){
      nav.classList.toggle('open-nav');
      menu.classList.toggle("change-menu")
@@ -19,3 +22,8 @@ buttonLeft.addEventListener("click",()=>{
     specialContainer.scrollLeft -= 200
 });
 
+
+inputButton.addEventListener("click",function(){
+    console.log('torch')
+      input.value=""
+});
